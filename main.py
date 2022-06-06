186ca49e02969eff7c8603ef0dc7f92bb4473ab3
Shreetik Sahani <shreetikkumar@gmail.com>
2022-06-07 02:26:11 +0530

    3rd commit

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from asyncio.windows_events import NULL
 from distutils.log import debug
-from flask import Flask, render_template
+from email import message
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_recaptcha import ReCaptcha
+from requests import post
+import mysql.connector
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+conn = NULL
+try:
+    conn = mysql.connector.connect(host="localhost", user="root",
+                                   password="root", database="onehealth", auth_plugin='mysql_native_password')
+except:
+    print("error")
+
+cursor = conn.cursor()
+
 app.config['RECAPTCHA_SITE_KEY'] = '6LdQ4DwgAAAAAACoU2jSbZUSR4Fvi6CySiJ75BFx'
 app.config['RECAPTCHA_SECRET_KEY'] = '6LdQ4DwgAAAAAFxykuQHImTwFb8fcpPLLwSpe8O4'
 
@@ -26,7 +41,72 @@ def login():
 
 @ app.route('/user')
 def userpanel():
-    return render_template('userdashboard.html')
+    if 'user_name' in session:
+        return render_template('userdashboard.html')
+    else:
+        return redirect('/login')
+
+
+@ app.route('/createuser', methods=['POST'])
+def createUser():
+    fname = request.form.get('fname')
+    uname = request.form.get('uname')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    password = request.form.get('password')
+    gender = request.form.get('gender')
+
+    cursor.execute(
+        """SELECT * FROM `user` WHERE `email_id` LIKE '{}' """.format(email))
+    user = cursor.fetchall()
+
+    if len(user) == 0:
+
+        cursor.execute("""INSERT INTO `user` (`user_id`,`full_name`,`user_name`,`email_id`,`phone_no`,`gender`,`password`) VALUES(NULL,'{}','{}','{}','{}','{}','{}')""".format(
+            fname, uname, email, phone, gender, password))
+
+        conn.commit()
+
+        if cursor.rowcount == 1:
+            flash(f"Account Succesfully created", "success")
+        else:
+            flash(f"Server Error", "danger")
+
+        return redirect('/signup')
+
+    else:
+        flash(f"User already present", "warning")
+
+        return redirect('/signup')
+
+
+@ app.route('/validate_login', methods=['POST'])
+def validateLogin():
+    if recaptcha.verify():
+        email = request.form.get('txtmail')
+        password = request.form.get('txtpass')
+
+        cursor.execute(
+            """SELECT * FROM `user` WHERE `email_id` LIKE '{}' AND `password` LIKE '{}'""".format(email, password))
+        user = cursor.fetchall()
+
+        if len(user) > 0:
+            session['user_name'] = user[0][2]
+            return redirect('/user')
+        else:
+            flash(f"Invalid user credentials", "error")
+            return redirect('/login')
+
+        return "{}{}".format(email, password)
+    else:
+        message = "Please fill out the ReCaptcha!"
+        return render_template('login.html', message=message)
+
+
+@ app.route('/logout')
+def logout():
+    session.pop('user_name')
+    return redirect('/login')
 
 
 if __name__ == "__main__":
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+@ app.route('/createuser', methods=['POST'])
+# def createUser():
+#     fname = request.form.get('fname')
+#     uname = request.form.get('uname')
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+#     gender = request.form.get('gender')
+
+#     return "{} {} {} {} {}".format(fname, uname, email, password, gender)
